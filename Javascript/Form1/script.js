b79764e819a358035748ca72d567de3b27d09509
Amrutha0310 <amruthamohandas1410@gmail.com>
2025-12-11 20:34:36 +0530

    Form

---
+++ Javascript/Form1/script.js
@@ -0,0 +1,49 @@
+function Submit(){
+    const nm=document.getElementById("name").value.trim();
+    const em=document.getElementById("email").value.trim();
+    const ph=document.getElementById("phone").value.trim();
+    const db=document.getElementById("dob").value.trim();
+
+
+//validation
+if(!/^[A-Za-z ]+$/.test(nm)){   //Regular Expression
+   alert("Wrong Input");
+  return;
+ } 
+ if(!/^[\w.]+@gmail|outlook|ricr|yahoo)\.(com|in|co.in)$/.test(em)){
+     alert("Wrong Email");
+     return;
+ }
+  
+   
+
+
+
+
+
+ const data={
+    FullName: nm,
+    Email: em,
+    Phone: ph,
+    DOB: db,
+};
+console.log(data);
+
+}
+
+ 
+
+    
+ 
+
+      
+
+
+ 
+   
+ 
+
+
+
+
+
