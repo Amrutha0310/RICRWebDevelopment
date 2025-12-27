38aa1ca680742b4451d430fe333811f9328ffdb2
Amrutha0310 <amruthamohandas1410@gmail.com>
2025-12-27 18:08:48 +0530

    react application

--- React/Demo/src/App.jsx
+++ React/Demo/src/App.jsx
@@ -3,36 +3,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { BrowserRouter,Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Contact from"./pages/Contact";
+import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Product from "./pages/Product";
 
-
 function App() {
-  // let a = 5;
-  // let b = 10;
-  // console.log(a + b);
-
   return (
     <>
       <BrowserRouter>
-      <Header/>
-     <Routes>
-       <Route  path="/" element={<Home />} />
-       <Route  path="/about" element={<About />} />
-       <Route  path="/contact" element={<Contact />} />
-       <Route  path="/product" element={<Product />} />
-       
-     </Routes>
-     <Footer/>
+        <Header />
 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
       </BrowserRouter>
-   
     </>
   );
 }
--- React/Demo/src/components/Header.jsx
+++ React/Demo/src/components/Header.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 
-
-
-//functional components->arrow function it acts like function 
+//functional components->arrow function it acts like function
 const Header = () => {
-    return(
-        <>
-             <div className="d-flex justify-content-between align-items-center p-2 bg-primary">
-                 <h3>My Website</h3>
-                 <div className="d-flex gap-3">
-                    <Link to={"/"}className="text-decoration-none"></Link>
-                 </div>
-             </div>
-        </>
-        
-    )
+  return (
+    <>
+      <div className="d-flex justify-content-between align-items-center p-2 bg-primary">
+        <h3>My Website</h3>
+        <div className="d-flex gap-3">
+          <Link to={"/"} className="text-decoration-none text-light">
+            Home
+          </Link>
+          <Link to={"/about"} className="text-decoration-none text-light">
+            About
+          </Link>
+          <Link to={"/product"} className="text-decoration-none text-light">
+            Product
+          </Link>
+          <Link to={"/contact"} className="text-decoration-none text-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Header;
